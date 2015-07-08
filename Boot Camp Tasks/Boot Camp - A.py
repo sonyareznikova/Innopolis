ed0f239063f20c259a33a3a0fea93aef37a6d6c4
Constantine <gogolgrind@yandex.ru>
2015-07-08 23:11:24 +0300

    Second commit
    Task A: Add messages about  solution

--- Boot Camp Tasks/Boot Camp - A.py
+++ Boot Camp Tasks/Boot Camp - A.py
@@ -9,33 +9,33 @@ def main():
         if b == 0:
             if c == 0:
                 ## There are inf roots
-                sys.stdout.write('-1 \n')
+                sys.stdout.write('There are inf roots \n')
             else:
                 ## There are no roots
-                sys.stdout.write('0 \n')
+                sys.stdout.write('There are no any roots \n')
         else:
             ## There is one root
             x = ((c/b) * (-1.0))
             ## 0 2 0 -> -0.0 ???
             if x == -0.0:
                 x = 0.0
-            sys.stdout.write('1 \n%.5f \n'% x)
+            sys.stdout.write('One Real Root  \n%.5f \n'% x)
     elif a != 0:
         d = b*b - 4*a*c
         if d < 0:
             ## There are complex roots
-            sys.stdout.write('0\n')
+            sys.stdout.write('There are no real roots \n')
         elif d == 0:
             ## There is one root
             x = -b/2/a
-            sys.stdout.write('1 \n%.5f \n'% x)
+            sys.stdout.write('One Real Root \n%.5f \n'% x)
         elif d > 0:
             ## There are two roots
             x1 = (-b + d**0.5)/2/a
             x2 = (-b - d**0.5)/2/a
             if x1 > x2:
                 x1,x2 = x2,x1
-            sys.stdout.write('2 \n%.5f \n%.5f \n'% (x1,x2))
+            sys.stdout.write('Two Real Roots \n%.5f \n%.5f \n'% (x1,x2))
 if __name__ == "__main__":
     ##sys.stdin = open('f.in','r')
     ##sys.stdout = open('f.out','w')
